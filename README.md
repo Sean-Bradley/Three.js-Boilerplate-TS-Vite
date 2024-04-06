828e6803407d1e4b4315fc0c0be6588879af33a6
Sean Bradley <seanwasere@gmail.com>
2024-04-06 10:53:21 +0100

    updates

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Three.js-Boilerplate-TS-Vite
+
+A Three.js Boilerplate for TypeScript Vite projects.
+
+This boilerplate is supplementary to <!--my book titled [**Three.js and TypeScript**](https://amzn.to/3FahROZ) and--> my **ThreeJS and TypeScript** courses at [Udemy](https://www.udemy.com/course/threejs-tutorials/?referralCode=4C7E1DE91C3E42F69D0F) and [YouTube (Channel membership required)](https://www.youtube.com/playlist?list=PLKWUX7aMnlEKTmkBqwjc-tZgULJdNBjEd)
+
+It is only just the beginning of what you will create.
+
+Course Discount Coupons : https://sbcode.net/coupons#threejs
+
+## Boilerplate Overview
+
+When run, the boilerplate shows a multi-coloured wireframe cube, with `OrbitControls`, `Dat.GUI` and `Stats.js` included.
+
+[Example](https://sbcode.net/threejs/threejs-typescript-boilerplate/)
+
+![](docs/screengrab.jpg)
+
+## Installing
+
+```bash
+git clone https://github.com/Sean-Bradley/Three.js-Boilerplate-TS-Vite.git
+cd Three.js-Boilerplate-TS-Vite
+npm install
+npm run dev
+```
+
+[Visit ](http://localhost:5173/)
+
+To create production version,
+
+```bash
+npm run build
+npm preview
+```
+
+[Visit ](http://localhost:4173/)
+
+
---
+++ src/main.ts
@@ -0,0 +1,56 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import Stats from 'three/addons/libs/stats.module.js'
+import { GUI } from 'dat.gui'
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 1.5
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+new OrbitControls(camera, renderer.domElement)
+
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshNormalMaterial({ wireframe: true })
+
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+const gui = new GUI()
+
+const cubeFolder = gui.addFolder('Cube')
+cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+cubeFolder.open()
+
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position, 'z', 0, 20)
+cameraFolder.open()
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  //cube.rotation.x += 0.01
+  //cube.rotation.y += 0.01
+
+  renderer.render(scene, camera)
+
+  stats.update()
+}
+
+animate()
