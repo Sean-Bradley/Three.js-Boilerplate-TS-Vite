8db3d12e055c76de79b272c1c3d34cd2ce876ed2
Kyoya67 <jackma8569@gmail.com>
2024-08-14 12:52:39 +0900

    Add my email

--- src/main.ts
+++ src/main.ts
@@ -62,12 +62,6 @@ gui.add(setScene, 'sceneA')
 gui.add(setScene, 'sceneB')
 gui.add(setScene, 'sceneC')
 
-// const cubeFolder = gui.addFolder('Cube')
-// cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-// cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-// cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-// cubeFolder.open()
-
 const cameraFolder = gui.addFolder('Camera')
 cameraFolder.add(camera.position, 'z', 0, 20)
 cameraFolder.open()
