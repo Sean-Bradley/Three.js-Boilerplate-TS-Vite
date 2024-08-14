eec372a36b104159c2340925059ac4cc3525c7cc
Kyoya <86813838+Kyoya67@users.noreply.github.com>
2024-08-14 12:39:01 +0900

    Scene Practice

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import Stats from 'three/addons/libs/stats.module.js'
 import { GUI } from 'dat.gui'
 
-const scene = new THREE.Scene()
+const sceneA = new THREE.Scene()
+sceneA.background = new THREE.Color(0x123456)
+
+const sceneB = new THREE.Scene()
+sceneB.background = new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')
+
+const sceneC = new THREE.Scene()
+sceneC.background = new THREE.CubeTextureLoader().setPath('https://sbcode.net/img/').load(['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png'])
+sceneC.backgroundBlurriness = 0.25
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 1.5
@@ -24,19 +32,41 @@ new OrbitControls(camera, renderer.domElement)
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshNormalMaterial({ wireframe: true })
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+const cubeA = new THREE.Mesh(geometry, material)
+sceneA.add(cubeA)
+
+const cubeB = new THREE.Mesh(geometry, material)
+sceneB.add(cubeB)
+
+const cubeC = new THREE.Mesh(geometry, material)
+sceneC.add(cubeC)
 
 const stats = new Stats()
 document.body.appendChild(stats.dom)
 
-const gui = new GUI()
+let activeScene = sceneA
+const setScene = {
+  sceneA: () => {
+    activeScene = sceneA
+  },
+  sceneB: () => {
+    activeScene = sceneB
+  },
+  sceneC: () => {
+    activeScene = sceneC
+  },
+}
 
-const cubeFolder = gui.addFolder('Cube')
-cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-cubeFolder.open()
+const gui = new GUI()
+gui.add(setScene, 'sceneA')
+gui.add(setScene, 'sceneB')
+gui.add(setScene, 'sceneC')
+
+// const cubeFolder = gui.addFolder('Cube')
+// cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+// cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+// cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+// cubeFolder.open()
 
 const cameraFolder = gui.addFolder('Camera')
 cameraFolder.add(camera.position, 'z', 0, 20)
@@ -48,7 +78,7 @@ function animate() {
   //cube.rotation.x += 0.01
   //cube.rotation.y += 0.01
 
-  renderer.render(scene, camera)
+  renderer.render(activeScene, camera)
 
   stats.update()
 }
