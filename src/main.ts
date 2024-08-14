06592f9d9ab387934a746e30fe1eb9b6a8540271
Kyoya67 <jackma8569@gmail.com>
2024-08-14 13:05:02 +0900

    delete rotate

--- src/main.ts
+++ src/main.ts
@@ -69,9 +69,6 @@ cameraFolder.open()
 function animate() {
   requestAnimationFrame(animate)
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
-
   renderer.render(activeScene, camera)
 
   stats.update()
